c5100c5b6b51e5d176df28a39600b218714e46c9
VulqyFR <vulqy.pro@gmail.com>
2024-12-03 08:35:23 +0100

    aoc:3 first star completed

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,14 +1,14 @@
-use aoc::day02;
+use aoc::day03;
 
 fn main() -> std::io::Result<()> {
-    println!("Advent of Code 2024 - Day 2 - https://adventofcode.com/2024/day/1 \n\n");
+    println!("Advent of Code 2024 - Day 3 - https://adventofcode.com/2024/day/3 \n\n");
     println!("First star: \n");
     let start = std::time::Instant::now();
-    day02::first_star()?;
+    day03::first_star()?;
     println!("Elapsed time: {:?}\n\n", start.elapsed());
     println!("Second star:\n");
     let start = std::time::Instant::now();
-    day02::second_star()?;
+    day03::second_star()?;
     println!("Elapsed time: {:?}\n", start.elapsed());
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,7 @@ mod day01;
 mod day02;
 
 fn main() -> std::io::Result<()> {
-    println!("\n🎄 Welcome to my Advent of Code 2024 solutions in Rust 🦀!");
-    println!("I hope you enjoy the code and learn something new with me \n\n");
-    println!("Advent of Code 2024 - Day 1 - https://adventofcode.com/2024/day/1 \n");
-    println!("First star: \n");
-    let start = std::time::Instant::now();
-    day01::first_star()?;
-    println!("Elapsed time: {:?}\n\n", start.elapsed());
-    println!("Second star:\n");
-    let start = std::time::Instant::now();
-    day01::second_star()?;
-    println!("Elapsed time: {:?}\n", start.elapsed());
-
-    println!("First star: \n");
-    let start = std::time::Instant::now();
-    day02::first_star()?;
-    println!("Elapsed time: {:?}\n\n", start.elapsed());
-    println!("Second star:\n");
-    let start = std::time::Instant::now();
-    day02::second_star()?;
-    println!("Elapsed time: {:?}\n", start.elapsed());
+    println!("\n🎄 Welcome to my Advent of Code 2024 solutions in Rust 🦀!\n");
+    println!("Run : cargo run --bin day<number> to see my solution");
     Ok(())
 }
