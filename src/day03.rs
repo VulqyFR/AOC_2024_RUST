b36e1125e71d13fc3dbff594773141914f4f0eb7
VulqyFR <vulqy.pro@gmail.com>
2024-12-05 08:39:30 +0100

    aoc3: completed

--- src/day03.rs
+++ src/day03.rs
@@ -1,111 +1,105 @@
-use std::{
-    fs::File,
-    io::{BufRead, BufReader},
-};
+use std::fs::File;
+use std::io::{self, BufRead};
 
-struct Multiplication {
-    first: i32,
-    second: i32,
+pub fn read_file(filename: &str, part: u32) -> Result<(), io::Error> {
+    let file = File::open(filename)?;
+    let reader = io::BufReader::new(file);
+    let mut sum = 0;
+    let mut enabled = true;
+
+    if part == 1 {
+        for line in reader.lines() {
+            sum += process_line_part1(&line?);
+        }
+    } else {
+        for line in reader.lines() {
+            let (line_sum, new_enabled) = process_line_part2(&line?, enabled);
+            sum += line_sum;
+            enabled = new_enabled;
+        }
+    }
+
+    println!("Part {}: {}", part, sum);
+    Ok(())
 }
 
-fn is_instruction(line: &str, index: usize, instruction: &str) -> bool {
-    if index + instruction.len() > line.len() {
-        return false;
+fn parse_number(chars: &[char], start: usize) -> Option<(i64, usize)> {
+    let mut end = start;
+    while end < chars.len() && chars[end].is_ascii_digit() {
+        end += 1;
     }
-    &line[index..index + instruction.len()] == instruction
-}
-
-fn parse_multiplication(line: &str, index: usize) -> Option<Multiplication> {
-    if index + 4 > line.len() || &line[index..index + 4] != "mul(" {
-        return None;
-    }
-
-    let mut num_str = String::new();
-    let mut i = index + 4;
-    let mut first_digit = 0;
-    let mut second_digit = 0;
-
-    while i < line.len() {
-        let char = line.chars().nth(i).unwrap();
-        if char == ',' {
-            if let Ok(num) = num_str.parse::<i32>() {
-                first_digit = num;
-                num_str = String::new();
-                i += 1;
-                break;
-            }
-            return None;
+    if end > start && end - start <= 3 {
+        let num_str: String = chars[start..end].iter().collect();
+        if let Ok(num) = num_str.parse::<i64>() {
+            return Some((num, end));
         }
-        if !char.is_digit(10) {
-            return None;
-        }
-        num_str.push(char);
-        i += 1;
-    }
-
-    while i < line.len() {
-        let char = line.chars().nth(i).unwrap();
-        if char == ')' {
-            if let Ok(num) = num_str.parse::<i32>() {
-                second_digit = num;
-                return Some(Multiplication {
-                    first: first_digit,
-                    second: second_digit,
-                });
-            }
-            return None;
-        }
-        if !char.is_digit(10) {
-            return None;
-        }
-        num_str.push(char);
-        i += 1;
     }
     None
 }
 
-fn read_file(path: &str, star: u8) -> Result<(), std::io::Error> {
-    let file = File::open(path)?;
-    let buf_reader = BufReader::new(file);
+fn process_line_part1(line: &str) -> i64 {
+    let chars: Vec<char> = line.chars().collect();
     let mut sum = 0;
+    let mut i = 0;
 
-    for line in buf_reader.lines() {
-        let line = line?;
-        if star == 1 {
-            for index in 0..line.len() {
-                if let Some(mult) = parse_multiplication(&line, index) {
-                    sum += mult.first * mult.second;
+    while i < chars.len() - 4 {
+        if chars[i..].starts_with(&['m', 'u', 'l', '(']) {
+            i += 4;
+            if let Some((x, next_pos)) = parse_number(&chars, i) {
+                i = next_pos;
+                if i < chars.len() && chars[i] == ',' {
+                    i += 1;
+                    if let Some((y, next_pos)) = parse_number(&chars, i) {
+                        i = next_pos;
+                        if i < chars.len() && chars[i] == ')' {
+                            sum += x * y;
+                        }
+                    }
                 }
             }
         } else {
-            let mut muls_enabled = true;
-            let mut index = 0;
+            i += 1;
+        }
+    }
+    sum
+}
 
-            while index < line.len() {
-                if is_instruction(&line, index, "do()") {
-                    muls_enabled = true;
-                    index += 4;
-                } else if is_instruction(&line, index, "don't()") {
-                    muls_enabled = false;
-                    index += 7;
-                } else {
-                    if muls_enabled {
-                        if let Some(mult) = parse_multiplication(&line, index) {
-                            sum += mult.first * mult.second;
-                            while index < line.len() && line.chars().nth(index).unwrap() != ')' {
-                                index += 1;
-                            }
-                            index += 1;
+fn process_line_part2(line: &str, mut enabled: bool) -> (i64, bool) {
+    let chars: Vec<char> = line.chars().collect();
+    let mut sum = 0;
+    let mut i = 0;
+
+    while i < chars.len() {
+        if i + 3 < chars.len() && chars[i..].starts_with(&['d', 'o', '(', ')']) {
+            enabled = true;
+            i += 4;
+        } else if i + 6 < chars.len()
+            && chars[i..].starts_with(&['d', 'o', 'n', '\'', 't', '(', ')'])
+        {
+            enabled = false;
+            i += 7;
+        } else if enabled && i + 3 < chars.len() && chars[i..].starts_with(&['m', 'u', 'l', '(']) {
+            i += 4;
+            if let Some((x, next_pos)) = parse_number(&chars, i) {
+                i = next_pos;
+                if i < chars.len() && chars[i] == ',' {
+                    i += 1;
+                    if let Some((y, next_pos)) = parse_number(&chars, i) {
+                        i = next_pos;
+                        if i < chars.len() && chars[i] == ')' {
+                            sum += x * y;
+                            i += 1;
                             continue;
                         }
                     }
-                    index += 1;
                 }
             }
+            i += 1;
+        } else {
+            i += 1;
         }
     }
-    println!("Sum for star {}: {}", star, sum);
-    Ok(())
+    (sum, enabled)
 }
 
 pub fn first_star() -> Result<(), std::io::Error> {
---
+++ src/day03_old.rs
@@ -0,0 +1,138 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+struct Multiplication {
+    first: i32,
+    second: i32,
+}
+
+fn is_instruction(line: &str, index: usize, instruction: &str) -> bool {
+    if index + instruction.len() > line.len() {
+        return false;
+    }
+    &line[index..index + instruction.len()] == instruction
+}
+
+fn parse_multiplication(line: &str, index: usize) -> Option<Multiplication> {
+    if index + 4 > line.len() || &line[index..index + 4] != "mul(" {
+        return None;
+    }
+
+    let mut num_str = String::new();
+    let mut i = index + 4;
+    let mut first_digit = 0;
+    let mut second_digit = 0;
+
+    while i < line.len() {
+        let char = line.chars().nth(i).unwrap();
+        if char == ',' {
+            if let Ok(num) = num_str.parse::<i32>() {
+                first_digit = num;
+                num_str = String::new();
+                i += 1;
+                break;
+            }
+            return None;
+        }
+        if !char.is_digit(10) {
+            return None;
+        }
+        num_str.push(char);
+        i += 1;
+    }
+
+    while i < line.len() {
+        let char = line.chars().nth(i).unwrap();
+        if char == ')' {
+            if let Ok(num) = num_str.parse::<i32>() {
+                second_digit = num;
+                return Some(Multiplication {
+                    first: first_digit,
+                    second: second_digit,
+                });
+            }
+            return None;
+        }
+        if !char.is_digit(10) {
+            return None;
+        }
+        num_str.push(char);
+        i += 1;
+    }
+    None
+}
+
+fn read_file(path: &str, star: u8) -> Result<(), std::io::Error> {
+    let file = File::open(path)?;
+    let buf_reader = BufReader::new(file);
+    let mut sum = 0;
+
+    for line in buf_reader.lines() {
+        let line = line?;
+        if star == 1 {
+            for index in 0..line.len() {
+                if let Some(mult) = parse_multiplication(&line, index) {
+                    sum += mult.first * mult.second;
+                }
+            }
+        } else {
+            let mut muls_enabled = true;
+            let mut index = 0;
+
+            while index < line.len() {
+                if is_instruction(&line, index, "do()") {
+                    muls_enabled = true;
+                    index += 4;
+                } else if is_instruction(&line, index, "don't()") {
+                    muls_enabled = false;
+                    index += 7;
+                } else {
+                    if muls_enabled {
+                        if let Some(mult) = parse_multiplication(&line, index) {
+                            println!(
+                                "Processing enabled mul at {}: {}*{} = {}",
+                                index,
+                                mult.first,
+                                mult.second,
+                                mult.first * mult.second
+                            );
+                            sum += mult.first * mult.second;
+                            while index < line.len() && line.chars().nth(index).unwrap() != ')' {
+                                index += 1;
+                            }
+                            index += 1;
+                            continue;
+                        }
+                    } else {
+                        if let Some(mult) = parse_multiplication(&line, index) {
+                            println!(
+                                "Skipping disabled mul at {}: {}*{}",
+                                index, mult.first, mult.second
+                            );
+                            while index < line.len() && line.chars().nth(index).unwrap() != ')' {
+                                index += 1;
+                            }
+                            index += 1;
+                            continue;
+                        }
+                    }
+                    index += 1;
+                }
+            }
+        }
+    }
+    println!("Sum for star {}: {}", star, sum);
+    Ok(())
+}
+
+pub fn first_star() -> Result<(), std::io::Error> {
+    read_file("src/inputs/day03.txt", 1)?;
+    Ok(())
+}
+
+pub fn second_star() -> Result<(), std::io::Error> {
+    read_file("src/inputs/day03.txt", 2)?;
+    Ok(())
+}
